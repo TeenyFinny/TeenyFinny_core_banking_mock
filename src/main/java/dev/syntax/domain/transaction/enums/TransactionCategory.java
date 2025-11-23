d60c80c21e0d97be0424175266cb26cb021fd693
양은서 <dmstj4871@naver.com>
2025-11-24 00:20:16 +0900

    feat: 트랜잭션 관련 ENUM 정리

---
+++ src/main/java/dev/syntax/domain/transaction/enums/TransactionCategory.java
@@ -0,0 +1,5 @@
+package dev.syntax.domain.transaction.enums;
+
+public enum TransactionCategory {
+    SHOPPING, EDU, TRANSPORT, TRANSFER, ENT, FOOD, ETC
+}
---
+++ src/main/java/dev/syntax/domain/transaction/enums/TransactionCode.java
@@ -0,0 +1,11 @@
+package dev.syntax.domain.transaction.enums;
+
+public enum TransactionCode {
+    DEPOSIT,            // 입금
+    WITHDRAW,           // 출금
+    AUTO_DEPOSIT,       // 자동이체 입금
+    AUTO_WITHDRAW,      // 자동이체 출금
+    INVESTMENT_IN,      // 투자 입금
+    INVESTMENT_OUT,     // 투자 출금
+}
+
