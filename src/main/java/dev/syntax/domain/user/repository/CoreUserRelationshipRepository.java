09a49bd1892b58aff401e1074849013a4caf2c50
yes2489 <yes24150@gmail.com>
2025-11-24 16:19:45 +0900

    feat: 가족 관계 매핑 로직 구현

--- src/main/java/dev/syntax/domain/user/repository/CoreUserRelationshipRepository.java
+++ src/main/java/dev/syntax/domain/user/repository/CoreUserRelationshipRepository.java
@@ -1,7 +1,17 @@
 package dev.syntax.domain.user.repository;
 
+import dev.syntax.domain.user.entity.CoreUser;
 import dev.syntax.domain.user.entity.CoreUserRelationship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CoreUserRelationshipRepository extends JpaRepository<CoreUserRelationship, Long> {
+
+    /**
+     * 부모와 자녀 간의 가족 관계가 이미 존재하는지 확인합니다.
+     *
+     * @param parent 부모 CoreUser
+     * @param child 자녀 CoreUser
+     * @return 관계가 존재하면 true, 없으면 false
+     */
+    boolean existsByParentAndChild(CoreUser parent, CoreUser child);
 }
