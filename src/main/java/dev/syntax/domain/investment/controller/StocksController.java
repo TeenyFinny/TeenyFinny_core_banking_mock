e76209952994361f3bcdef63e7351c27caa52788
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-11-25 17:17:56 +0900

    refactor: StocksController 반환타입 MultiPriceRes로 변경

--- src/main/java/dev/syntax/domain/investment/controller/StocksController.java
+++ src/main/java/dev/syntax/domain/investment/controller/StocksController.java
@@ -2,11 +2,7 @@ package dev.syntax.domain.investment.controller;
 
 import dev.syntax.external.kis.KisStockApiClient;
 import dev.syntax.external.kis.dto.MultiPriceRes;
-import dev.syntax.global.response.ApiResponseUtil;
-import dev.syntax.global.response.BaseResponse;
-import dev.syntax.global.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +12,8 @@ public class StocksController {
     private final KisStockApiClient kisStockApiClient;
 
     @GetMapping("/investments/stocks")
-    public ResponseEntity<BaseResponse<?>> getStocks() {
-        MultiPriceRes res = kisStockApiClient.getMultiPrice();
-        return ApiResponseUtil.success(SuccessCode.OK, res);
+    public MultiPriceRes getStocks() {
+        return kisStockApiClient.getMultiPrice();
     }
 
 }
