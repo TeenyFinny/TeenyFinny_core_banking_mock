441b1cbab868d173da4d8f85fdc07b13356a25c4
hyojeongbae <bhj09270@naver.com>
2025-11-25 23:41:56 +0900

    feat: 특정 계좌 거래내역 조회 후 날짜 내림차순 정렬

--- src/main/java/dev/syntax/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/dev/syntax/domain/transaction/repository/TransactionRepository.java
@@ -2,6 +2,15 @@ package dev.syntax.domain.transaction.repository;
 
 import dev.syntax.domain.transaction.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByAccount_IdOrderByTransactionDateDesc(Long AccountId);
+
+    @Query("select a from Transaction a where a.account.number = :number")
+    List<Transaction> findByNumberdOrderByTransactionDateDesc(@Param("number") String number);
 }
