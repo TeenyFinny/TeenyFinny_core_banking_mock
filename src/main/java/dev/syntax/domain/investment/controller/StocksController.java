210c59797fdeafff6d9641ceea4d9308f74170a9
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-11-26 10:38:13 +0900

    refactor:줏ã주식리스트 ãurl 변경

--- src/main/java/dev/syntax/domain/investment/controller/StocksController.java
+++ src/main/java/dev/syntax/domain/investment/controller/StocksController.java
@@ -2,11 +2,7 @@ package dev.syntax.domain.investment.controller;
 
 import dev.syntax.external.kis.KisStockApiClient;
 import dev.syntax.external.kis.dto.MultiPriceRes;
-import dev.syntax.global.response.ApiResponseUtil;
-import dev.syntax.global.response.BaseResponse;
-import dev.syntax.global.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class StocksController {
     private final KisStockApiClient kisStockApiClient;
 
-    @GetMapping("/investments/stocks")
-    public ResponseEntity<BaseResponse<?>> getStocks() {
-        MultiPriceRes res = kisStockApiClient.getMultiPrice();
-        return ApiResponseUtil.success(SuccessCode.OK, res);
+    @GetMapping("/core/investments/stocks")
+    public MultiPriceRes getStocks() {
+        return kisStockApiClient.getMultiPrice();
     }
 
 }
