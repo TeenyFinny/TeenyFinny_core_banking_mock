d154213d33874a282fa235b0a0be047ae3b7bcc5
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-11-26 19:00:56 +0900

    fix: 주식 리스트 endpoint 수정

--- src/main/java/dev/syntax/domain/investment/controller/StocksController.java
+++ src/main/java/dev/syntax/domain/investment/controller/StocksController.java
@@ -4,14 +4,16 @@ import dev.syntax.external.kis.KisStockApiClient;
 import dev.syntax.external.kis.dto.MultiPriceRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/core/investments")
 public class StocksController {
     private final KisStockApiClient kisStockApiClient;
-  
-    @GetMapping("/core/investments/stocks")
+
+    @GetMapping("/stocks")
     public MultiPriceRes getStocks() {
         return kisStockApiClient.getMultiPrice();
     }
